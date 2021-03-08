0979bd4b859a7b13db94fcd03fa74e3117af9466
And0p <And0p@192.168.10.117>
2021-03-08 15:18:13 +0100

    DisplayDevice - Done

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -3,7 +3,7 @@ package no.hvl.dat110.iotsystem;
 import no.hvl.dat110.client.Client;
 import no.hvl.dat110.messages.Message;
 import no.hvl.dat110.messages.PublishMsg;
-import no.hvl.dat110.common.TODO;
+
 
 public class DisplayDevice {
 	
@@ -17,6 +17,18 @@ public class DisplayDevice {
 				
 		// create a client object and use it to
 		
+		Client client = new Client("display", Common.BROKERHOST, Common.BROKERPORT);
+		client.connect();
+		client.createTopic(Common.TEMPTOPIC);
+		client.subscribe(Common.TEMPTOPIC);
+		
+		for (int i = 0; i < COUNT; i ++) {
+			client.receive();
+		}
+		
+		client.unsubscribe("Temperature");
+		client.disconnect();
+		
 		// - connect to the broker
 		// - create the temperature topic on the broker
 		// - subscribe to the topic
@@ -24,11 +36,11 @@ public class DisplayDevice {
 		// - unsubscribe from the topic
 		// - disconnect from the broker
 		
+	
 		// TODO - END
 		
 		System.out.println("Display stopping ... ");
 		
-		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 }
